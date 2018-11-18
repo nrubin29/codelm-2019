23691f03a217cc4a7ef8b87cb2ba0d5d08737051
nrubin29 <nrubin29@gmail.com>
2018-11-18 17:34:56 -0500

    Missing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './common/views/login/login.component';
+import { DisconnectedComponent } from './common/views/disconnected/disconnected.component';
+import { RegisterComponent } from './common/views/register/register.component';
+import { SharedModule } from './shared.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EndComponent } from './competition/views/end/end.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DisconnectedComponent,
+    RegisterComponent,
+    EndComponent
   ],
   imports: [
-    BrowserModule
+    SharedModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
